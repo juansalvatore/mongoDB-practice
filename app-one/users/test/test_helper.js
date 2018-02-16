5b7b5ef857108c42c8e6b08e78bba147ba4805fb
Juan Salvatore <juansalvatore@live.com.ar>
2018-02-16 18:29:12 -0300

    Create User schema and mocha test for saving users

--- app-one/users/test/test_helper.js
+++ app-one/users/test/test_helper.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
+mongoose.Promise = global.Promise
+
 mongoose.connect('mongodb://localhost/users_test')
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', error => {
-    console.warn('Warning', error)
+mongoose.connection.once('open', () => {}).on('error', error => {
+  console.warn('Warning', error)
+})
+
+beforeEach(done => {
+  mongoose.connection.collections.users.drop(() => {
+    // ready to run the next test
+    done()
   })
+})
