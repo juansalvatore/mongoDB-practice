cca62a1f2b544f054b3c6c36a62344db3a157388
Juan Salvatore <juansalvatore@live.com.ar>
2018-02-16 19:05:36 -0300

    Create mocha read test

--- app-one/users/test/test_helper.js
+++ app-one/users/test/test_helper.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/users_test')
-mongoose.connection.once('open', () => {}).on('error', error => {
-  console.warn('Warning', error)
+before(done => {
+  mongoose.connect('mongodb://localhost/users_test')
+  mongoose.connection
+    .once('open', () => {
+      done()
+    })
+    .on('error', error => {
+      console.warn('Warning', error)
+    })
 })
 
 beforeEach(done => {
