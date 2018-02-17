5e15b8808d4bd00e04e0083dabf43210ca176d2b
Juan Salvatore <juansalvatore@live.com.ar>
2018-02-17 14:30:24 -0300

    Create delete test

--- app-one/users/test/reading_test.js
+++ app-one/users/test/reading_test.js
@@ -13,11 +13,14 @@ describe('Reading users out of the database', () => {
 
   it('finds all users with the name of joe', done => {
     User.find({ name: 'Joe' }).then(users => {
-      console.log(' ')
-      console.log('JOES ID: ', joe._id)
-      console.log('USERS ID: ', users[0]._id)
-      console.log(' ')
       assert(users[0]._id.toString() === joe._id.toString())
+      done()
+    })
+  })
+
+  it('find a user with a particular id', done => {
+    User.findOne({ _id: joe._id }).then(user => {
+      assert(user.name === 'Joe')
       done()
     })
   })
